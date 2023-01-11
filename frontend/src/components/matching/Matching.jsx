60188ed37f44da9a321697903362334f8167e89e
FreddyLiu06 <103144599+FreddyLiu06@users.noreply.github.com>
2023-01-11 16:55:40 +0800

    temp

--- frontend/src/components/matching/Matching.jsx
+++ frontend/src/components/matching/Matching.jsx
@@ -1,11 +1,20 @@
 import './Matching.css';
+import { useState, useEffect } from 'react';
 
-function Matching() {
+function Matching({baseEmail}) {
+    // baseEmail is the email of the current user
+
+    const recommendedUsers = 0;
+
+    // Initial function to get things from DB
+    const init = () => {
+
+    }
 
     return (
     <div className = 'matching-container'>
         <h1>
-            Matching Interface: Put your stuff here
+            {baseEmail}
         </h1>
     </div>
     );
