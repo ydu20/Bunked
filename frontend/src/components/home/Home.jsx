aeb33e2562d8db5db4501b6ac8c80bc59e52c481
FreddyLiu06 <103144599+FreddyLiu06@users.noreply.github.com>
2023-06-27 11:05:39 -0400

    new matching frontend

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -3,10 +3,12 @@ import './Home.css';
 import { Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import axios from '../AxiosInstance';
-import { useNavigate } from 'react-router-dom';
-import Matching from "../matching/Matching";
+import { useNavigate, Routes, Route } from 'react-router-dom';
+import MatchingDetail from "../matchingDetail/MatchingDetail";
 import Waitroom from '../waitroom/Waitroom';
 import Matches from "../matches/Matches";
+import MatchingCard from '../matchingCard/MatchingCard';
+import Sidebar from '../Sidebar/Sidebar';
 
 
 function Home() {
@@ -59,13 +61,6 @@ function Home() {
     }, []);
 
 
-    const logout = async () => {
-        axios.delete('/logout').then(() => {
-            Cookies.remove('email');
-            navigate('/');
-        });
-    }
-
     if (!Cookies.get('email')) {
         return <Navigate to = "/"/>;
     } else if (!loading) {
@@ -80,34 +75,26 @@ function Home() {
             return (
                 <div className = "homepage-container">
 
-                    <div className = "sidebar-wrapper">
-                        <div className = "sidebar">
-                            <h1>Homepage</h1>
-                            <div>User: {` ${Cookies.get('email')}`}</div>
+                    <Sidebar setDisplayMode={setDisplayMode}/>
 
-                            <p>Options:</p>
-
-                            <button className = "temp-toggle" onClick = {() => {setDisplayMode(0)}}>Matching</button>
-
-                            <button className = "waitroom-toggle" onClick = {() => {setDisplayMode(1)}}>Waiting Room</button>
-
-                            <button className = "chat-toggle" onClick = {() => {setDisplayMode(2)}}>Chat</button>
-
-                            <button className = "home-logout" onClick = {logout}>Logout</button>
-
-                            <Matches baseEmail={Cookies.get('email')}/>
-                        </div>
-                    </div>
                     <div className = "interface-wrapper">
                         <div className = "interface-inner-wrapper">
-                            { recommendedUsers.length>0 && displayMode === 0 && 
+                            {/* { recommendedUsers.length>0 && displayMode === 0 && 
                                 <Matching baseEmail={Cookies.get('email')} userArr={recommendedUsers} lastIndex = {lastIndex} setLastIndex = {setLastIndex} resetRecommend={resetRecommend}/>
                             }
                             {
                                 displayMode === 1 && <Waitroom baseEmail={Cookies.get('email')}/>
                             }
-                            {// TO DO: ADD CHAT COMPONENT HERE
-                            }
+                            {
+                                displayMode === 2 && <MatchingCard baseEmail={Cookies.get('email')}></MatchingCard>
+                            } */}
+                            <Routes>
+                                <Route path="/" element={<MatchingCard />} />
+                                <Route path="/temp" element={<MatchingDetail baseEmail={Cookies.get('email')} userArr={recommendedUsers} lastIndex = {lastIndex} setLastIndex = {setLastIndex} resetRecommend={resetRecommend}/>} />
+
+                                <Route path="/matchingDetail" element={<MatchingDetail />}/>
+                                {/* <Route path="/chat" Chat component here />  */}
+                            </Routes>
                         </div>
                     </div>
                 </div>
---
+++ frontend/src/components/matchingDetail/MatchingDetail.jsx
@@ -0,0 +1,102 @@
+import './MatchingDetail.css';
+import { useState, useEffect } from 'react';
+import axios from '../AxiosInstance';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
+function MatchingDetail() {
+    // baseEmail is the email of the current user
+    const baseEmail = Cookies.get('email')
+    
+    // targetUser is the user dictionary that contains details about the user
+    const {state} = useLocation()
+    const targetUser = state.user
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+
+    }, [])
+
+
+    // // Function to get the next recommended user from recommendedUser array
+    // const nextRec = async () => {
+    //     if (index + 1 >= userArr.length) {
+    //         setLoading(true);
+    //         await resetRecommend();
+    //         setTimeout(() => {setIndex(0); setLoading(false)}, 500);
+    //     } else {
+    //         setIndex(index + 1);
+    //         setLastIndex(index + 1);
+    //     }
+    // }
+
+    // Method to check if there is a match between the newly accepted user and current user, and handle the match if there is
+    const handleMatch = async () => {
+        const match = (await axios.get('/actions/checkMatch', {params: {firstEmail: baseEmail, secEmail: targetUser.email}})).data; // Check if there is a match between the newly accepted user
+        if (match) {
+            alert("Matched!");
+            // TODO: Handle the match here, create new chat room/send new message
+        }
+    }
+
+    // Function to add user to waiting room
+    const addWaitRoom = () => {
+        axios.post('/actions/addUserWait', {baseEmail: baseEmail, targetEmail: targetUser.email})
+        .then(res => {
+            alert("Added to waiting room");
+        }).catch( err =>
+            alert('ERROR: ' + err)
+        )
+    }
+
+    // Function accept user
+    const acceptUser = () => {
+        // TODO: Need to implement checking if there is a match
+        axios.post('/actions/acceptUser', {baseEmail: baseEmail, targetEmail: targetUser.email})
+        .then(res => {
+            alert("User Accepted");
+            handleMatch(); // Check and handle match
+            navigate('/home')
+        }).catch(err => 
+            alert('ERROR: ' + err) 
+        )
+    }
+
+    // Reject user
+    const rejectUser = () => {
+        axios.post('/actions/rejectUser', {baseEmail: baseEmail, targetEmail: targetUser.email})
+        .then(res => {
+            alert("User Rejected");
+        }).catch(err => 
+            alert('ERROR: ' + err) 
+        )
+    }
+
+
+    return (      
+    <div className = 'matching-container'>
+        {
+            // TODO: Finish adding all user fields and styling
+        }
+        <h1>    
+            Name: {targetUser.name}
+        </h1>
+        <p>
+            Gender: {targetUser.gender}
+        </p>
+        <p>
+            Major: {targetUser.majors}
+        </p>
+        <p>
+            Hobbies: {targetUser.hobbies}
+        </p>
+        {/* <button onClick = {() => {addWaitRoom()}}>WaitRoom</button> */}
+        <button onClick = {() => {acceptUser()}}>Accept</button>
+        <button onClick = {() => {rejectUser()}}>Reject</button>
+    </div> 
+    
+    );
+}
+
+export default MatchingDetail;
